6bbfb9862466c9f3eb84ad29b7f1bbccf231df11
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-07-12 04:12:36 +0300

    + all blocks and css, still need mobile css and maybe some JS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 const menuItemLi = document.querySelectorAll('.header__menu-item');
 menuItemLi.forEach(li=>{
-  li.firstChild.addEventListener('click',(e)=>{
+  console.log(li)
+  li.firstElementChild.addEventListener('click',(e)=>{
 menuItemLi.forEach(li=>{
-  li.firstChild.classList.remove('a-active');
+  li.firstElementChild.classList.remove('a-active');
 })
+console.log(e.target)
 e.target.classList.add('a-active')
 
   })
 })
 
-//slider
-$(function(){
 
+$(function(){
+  //slider
   $(".slider").slick({
     arrows: true,
     dots: false,
@@ -29,4 +31,32 @@ $(function(){
       },
     ]
   });
+  //tabs
+  $( "#tabs" ).tabs();
+  //progress bars
+  let  createBar = (selector,value)=> {
+    $(selector).css("width",value)
+    $(selector).html(`<span>${value}</span>`)
+  };
+  createBar("#progress-1","80%");
+  createBar("#progress-2","30%");
+  createBar("#progress-3","70%");
+  createBar("#progress-4","60%");
+  // Scroll-anchor animation
+  $(".content, .contacts, .footer", ).on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 900);
+  });
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+  })
 });
